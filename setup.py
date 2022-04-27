7fd0ea23eb49240ede1e49d4bcc56d7105f674b5
Felix Kempf <42411016+fkempf92@users.noreply.github.com>
2022-04-27 10:33:53 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name='factordata',
+    version='1.0',
+    author='Felix Kempf',
+    author_email='felix.kempf@kcl.ac.uk',
+    description='Constructs 103 US firm-level firm characteristics and corresponding L/S portfolios',
+    url='https://github.com/fkempf92/taktile',
+    project_urls = {
+        "Bug Tracker": "https://github.com/fkempf92/FactorData/issues"
+    },
+    license='MIT',
+    packages=['factordata'],
+    install_requires=['datetime', 
+                      'bls >= 0.3.0', 
+                      'warnings',
+                      'json >= 2.0.9',
+                      'pandas >= 1.0.5',
+                      'numpy >= 1.18.5',
+                      'wrds >= 3.1.1',
+                      'scipy >= 1.5.0', 
+                      'statsmoderls >= 0.11.1',
+                      'fredapi >= 0.5.0'],
+)
